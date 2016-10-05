43f9f2d2ee1d7079b2468405b9ca1571ba131d70
AlexTol <atol514@gmail.com>
2016-10-05 11:06:41 -0700

    aaa
    aaa

--- src/JDBCProject.java
+++ src/JDBCProject.java
@@ -110,27 +110,27 @@ public class JDBCProject {
                 switch(userInput)
                 {
                     case 1:
-                        userSelect.add("*");
-                        Functions.DisplaySelected(stmt, userSelect, 1);
+                         userSelect.add("*");
+                         Functions.DisplaySelected(stmt, userSelect, 1);
                         break;
                     case 2:
-                        userSelect=Functions.getList(in, 1);
-                        Functions.DisplaySelected(stmt, userSelect, 1);
+                         userSelect=Functions.getList(in, 1);
+                         Functions.DisplaySelected(stmt, userSelect, 1);
                         break;
                     case 3: 
-                         userSelect.add("*");
-                         Functions.DisplaySelected(stmt, userSelect, 2);
+                          userSelect.add("*");
+                          Functions.DisplaySelected(stmt, userSelect, 2);
                         break;
                     case 4: 
-                        userSelect=Functions.getList(in, 2);
-                        Functions.DisplaySelected(stmt, userSelect, 2);
+                         userSelect=Functions.getList(in, 2);
+                         Functions.DisplaySelected(stmt, userSelect, 2);
                         break;
                     case 5: 
                         
                         break;
                     case 6: 
-                        userSelect=Functions.getList(in, 3);
-                        Functions.DisplaySelected(stmt, userSelect, 3);
+                         userSelect=Functions.getList(in, 3);
+                         Functions.DisplaySelected(stmt, userSelect, 3);
                         break;
                     case 7: 
                         break;
